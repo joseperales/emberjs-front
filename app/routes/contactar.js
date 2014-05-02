e7dfde1504702d12cc26f998f9f7e977e2f74a58
Jose Perales <jose.perales@gridsystems.com.ve>
2014-05-02 13:23:06 -0400

    Email Sentgit add .git add .

--- app/routes/contactar.js
+++ app/routes/contactar.js
@@ -1,5 +1,17 @@
-var ContactarRoute = Ember.Route.extend({
+var Person = Ember.Object.extend({
+    Name : "",
+    lastName : "",
+    email : "",
+    message : ""
+});
 
+var ContactarRoute = Ember.Route.extend({
+	model: function(){
+      return Person.create();
+	},
+    setupController : function(controller, model){
+        controller.set("model", model);
+    }
 });
 
 export default ContactarRoute;
