0053530881b04fd7c5fa877005e65675cc73deea
Jose Perales <jose.perales@gridsystems.com.ve>
2014-05-07 19:14:59 -0400

    Started Carasteristicas

--- app/controllers/contactar.js
+++ app/controllers/contactar.js
@@ -1,13 +1,14 @@
-var ContactarController = Ember.Controller.extend({
+//var ContactarController = Ember.Controller.extend({
+export default Ember.ObjectController.extend(Ember.Validations.Mixin,{
 	actions: {
-		Contacto: function() {			
+		Contacto: function() {	
+ 
+			//$('.massive.rocket.icon').transition('scale');
+				
 			var name = this.get('model.Name');
 			var lastName = this.get('model.lastName');
 			var email = this.get('model.email');
 			var message = this.get('model.message');
-			$('#button-send').html('Enviando el E-Mail...');
-			event.preventDefault();
-			
 			$.ajax({
 			type: 'POST',
 			url: '/front/send_form_email.php',
@@ -30,8 +31,65 @@ var ContactarController = Ember.Controller.extend({
 				$('#error').show();
 			}
 		});
-		this.transitionTo('social');
+		this.transitionToRoute('social');
 		}
-	}
+	},
+	validations: {
+		name: {
+			presence: { message: "Field is required." },
+			length: { minimum: 3, messages: { tooShort: "Must be at least 3 characters." } }
+		} 
+      //property1: {
+		//	format: { with: /^([A-Z]|\d)+$/, allowBlank: true, message: 'Value must comprise of capital letters and numbers only.'  }
+	//	}
+		/*, property2: {
+			length: { minimum: 2, messages: { tooShort: "Value must be at least 2 characters." } },
+			evilValidator: true
+		}
+		, property3: {
+			valueIsUnique: true
+		}*/
+		/*, property4: {
+			presence: { message: "Field is required." },
+			length: { minimum: 3, messages: { tooShort: "Must be at least 3 characters." } }
+		}
+		, property5: {
+			presence: { message: "Field is required." }
+		}*/
+	},
+
+	/*create: function () {
+		Ember.Logger.log("Send thing to server: ", this.get("model"));
+	},*/
+
+	debuggingErrors: function () {
+		var errors = this.get("errors");
+		var flattenedArray = [];
+		var properties = Ember.keys(errors);
+		//var messages;
+		//var modifiedMessages;
+
+		properties.forEach(function (property) {
+		var	messages = errors.get(property);
+		var	modifiedMessages = messages.map(function (message) {
+				return "%@: %@".fmt(property, message);
+			});
+
+			flattenedArray.pushObjects(modifiedMessages);
+		});
+
+		return flattenedArray.sort();
+	}.property("errors.name.@each"
+		//, "errors.property1.@each"
+		//, "errors.property2.@each"
+		//, "errors.property3.@each"
+		//, "errors.property4.@each"
+		//, "errors.property5.@each"
+	)
+	
 });
-export default ContactarController;
+//export default ContactarController;
+
+
+
+//});
--- app/routes/contactar.js
+++ app/routes/contactar.js
@@ -5,13 +5,26 @@ var Person = Ember.Object.extend({
     message : ""
 });
 
+var ThingModel = Ember.Object.extend({
+	name: ""
+	//property1: "",
+	//property2: "",
+	//property3: "",
+	//property4: ""
+	//property5: ""
+});
+
 var ContactarRoute = Ember.Route.extend({
 	model: function(){
       return Person.create();
 	},
     setupController : function(controller, model){
         controller.set("model", model);
-    }
+        controller.set("model", ThingModel.create());
+    }//,
+	//setupController: function (controller/*, model*/) {
+	//controller.set("model", ThingModel.create());
+	//}
 });
 
 export default ContactarRoute;
